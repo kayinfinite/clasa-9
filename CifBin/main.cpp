a76ce5df4ab74a4e9c23960df23592731e0862d0
kayinfinite <123296312+kayinfinite@users.noreply.github.com>
2023-01-22 12:49:37 +0200

    Update main.cpp

--- CifBin/main.cpp
+++ CifBin/main.cpp
@@ -5,10 +5,37 @@ using namespace std;
 int main()
 {
     int numar;
-    int zero;
-    int unu;
-    cin >> numar;
+    int zero = 0;
+    int decize;
+    int unu = 0;
+    //citim numarul de la tastatura
+    cout <<"Introduceti numarul: "; cin >> numar;
+    //verificam danca numarul e intre 1 si 1000000
     if(numar<1||numar>1000000){
-        cout << "Nu se poate";
+        cout << "Nu se poate introduce acest numar";
+        return 0;
     }
+    //incepem decodarea nr. binar
+    while(decize==1||decize==0){
+        //impartim la 2 sa gasim restul
+        decize = numar % 2;
+        //impartim numarul
+        numar = numar / 2;
+        //vedem ce numar binar si cate zerouri si unuri are
+        switch(decize){
+        case 0:
+            zero = zero + 1;
+            break;
+        case 1:
+            unu = unu + 1;
+            break;
+        }
+        //conditia de a se opri loop-ul
+        if(numar<1){
+            break;
+        }
+    }
+    cout <<"numarul de zero cifre: "<< zero <<"\n";
+    cout <<"numarul de unu cifre: "<< unu;
 }
+//linkul problemei: https://www.pbinfo.ro/probleme/429/cifbin
