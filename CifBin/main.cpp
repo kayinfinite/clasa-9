55e86b10332a704fefce408a08350cebbee920af
kayinfinite <alexandru.mocanu007@gmail.com>
2023-01-22 11:50:32 +0200

    lucrez la CifBin

--- CifBin/main.cpp
+++ CifBin/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int numar;
+    int zero;
+    int unu;
+    cin >> numar;
+    if(numar<1||numar>1000000){
+        cout << "Nu se poate";
+    }
 }
