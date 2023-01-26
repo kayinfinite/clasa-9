60bba51a8eb4035b960e6e6a4008da6c46c80ff9
kayinfinite <alexandru.mocanu007@gmail.com>
2023-01-26 20:47:11 +0200

    enter works

--- Free_time/FreeTime.cpp
+++ Free_time/FreeTime.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-
+#include <thread>
 using namespace std;
+
+int t = 0;
+
+static bool input = false;
+
+void speed(){
+    while(!input){
+    cout << "hello"<<"\n";
+    };
+
+}
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    // incerc sa facu un joculet cu tren in console aplication
+    std::thread worker(speed);
+    std::cin.get();
+    input = true;
+    worker.join();
+    std::cin.get();
+
 }
