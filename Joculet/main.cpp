63e6a2bfafbb313946b544456dde41080f9ad25f
kayinfinite <alexandru.mocanu007@gmail.com>
2023-02-15 19:44:24 +0200

    Box
    +cutie
    // acum joculetul are ce ii trebuie :D
    (Joculet.cpp)

--- Joculet/main.cpp
+++ Joculet/main.cpp
@@ -4,13 +4,18 @@ int main(){
     //mic joculet
     int position_player[2] = {3 ,3};
     // [0] pentru axa x; [1] pentru axa y;
+    int position_box[2] = {1 ,1};
     int condition = 0;
     char comanda;
 
     while(true){
         cout << "introduce comanda: "; cin >> comanda;
+        //comenzi de iscare
         if(comanda == 'w' && position_player[1] < 6 ){
                 position_player[1]++;
+                if((position_player[0]= position_box[0]) && (position_player[1]= position_box[1])){
+                    position_box[1]++;
+                }
         }
         if(comanda == 's' && position_player[1] > 0 ){
                 position_player[1]--;
@@ -21,6 +26,7 @@ int main(){
         if(comanda == 'd' && position_player[0] < 6 ){
                 position_player[0]++;
         }
-        cout <<"x ="<< position_player[0] <<"\n" <<"y ="<< position_player[1] <<"\n";
+        cout <<"Player location:"<<" x ="<< position_player[0] <<" y ="<< position_player[1] <<"\n";
+        cout <<"Box location:"<<" x ="<< position_box[0] <<" y ="<< position_box[1] <<"\n";
     }
 };
